d12a4f634857f09018433f9eab8f74b1f99b6259
tkpclark <tengkeping@gmail.com>
2013-06-19 07:53:14 +0800

    增加installAppList测试代码

--- src/test/tinkertest.cpp
+++ src/test/tinkertest.cpp
@@ -1,9 +1,14 @@
 #include "src/include/tinkertest.h"
 #include "src/include/applist.h"
 #include <QDebug>
+
 void testAppList()
 {
     AppList appList;
+
+
+    //test fetchApplist
+
     QList<AppInfo *> apppointerlist;
 
     apppointerlist = appList.fetchApplist(2);
@@ -23,4 +28,18 @@ void testAppList()
     }
 
     qDebug() << "list size: " << apppointerlist.size();
+
+
+
+    //test installApplist
+    QList<InstallationResult *> list;
+    list = appList.installApplist(apppointerlist);
+
+    QList<InstallationResult *>::iterator it1;
+    qDebug() << "result count: " << list.size();
+    for(it1 = list.begin(); it1 != list.end(); ++it1)
+    {
+        qDebug() << (*it1)->success;// << " " << (*it1)->message;
+    }
+
 }
